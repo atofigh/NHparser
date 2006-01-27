5cc9010d04b89d2dfc5522ee704438c8b78bffea
Ali Tofigh <alix.tofigh@gmail.com>
2006-01-27 16:20:39 +0000

    Revised the code for NHtester.
    - Bugfix: The program no longer crashes when given a bad filename.
    - Bugfix: The program now frees all allocated memory and closes opened
      files.
    - Improved error messages and error handling.
    - Improved output messages.
    - Added many more comments to the code.
    
    
    
    git-svn-id: file:///bioinfo/home/alix/scratch/NHparser.svn@6 529614df-3e0a-0410-b529-bb9e67687faa

--- NHtester.c
+++ NHtester.c
@@ -2,33 +2,70 @@
 #include <stdlib.h>
 #include "NHparser.h"
 
-static void
-write_tree_r (NHNode *n, FILE *out);
+static void write_tree_r (NHNode *n, FILE *out);
+static void write_tree (NHNode *root, char *file);
 
-static void
-write_tree (NHNode *root, char *file);
-
-extern int NH_12123_debug;
+/* See in-code comment about NH_12123_debug. */
+//extern int NH_12123_debug;
 
 int
 main (int argc, char *args[]) {
-    NHNode *root;
-    FILE *in, *err;
+    FILE *in = NULL;
+    NHNode *root = NULL;
 
+    /*
+     * Check correct invokation.
+     */
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s tree-file\n", args[0]);
-        exit(1);
+        fprintf(stderr, "Usage: NHtester <filename>\n");
+        exit(EXIT_FAILURE);
     } 
     
+    /*
+     * Open file and report errors.
+     */
     in = fopen(args[1], "r");
-    err = stderr;
+    if (!in) {
+        fprintf(stderr, "Could not open file '%s'.\n", args[1]);
+        perror(NULL);
+        exit(EXIT_FAILURE);
+    }
 
-    NH_12123_debug = 1;
-    root = NH_read_tree(in, err, args[1]);
+    /*
+     * To see debug output from lex and yacc set NH_12123_debug to
+     * one. NOTE! This is only for internal maintenance.
+     */
+    //    NH_12123_debug = 1;
+
+    /*
+     * Read the tree file. In case of syntax errors NH_read_tree will
+     * output error information on the provided error stream, and
+     * return NULL.
+     */
+    root = NH_read_tree(in, stderr, args[1]);
+    if (root == NULL) {
+        fprintf(stderr, "Error reading tree file.\nAborting.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * Here a correct tree must have been read. We will just output
+     * the tree structure in NHX-format.
+     */
+    printf("I managed to parse the following tree:\n");
     write_tree(root, NULL);
-    printf("\n");
-    //    printf("%d, %d\n", NH_get_lineno(), NH_get_colno());
-    return 0;
+    printf("\n\n");
+    printf("I detected end of tree at line %d and column %d.\n",
+           NH_get_lineno(), NH_get_colno());
+
+    /*
+     * Free used memory and close files.
+     */
+    NH_destroy_tree(root);
+    root = NULL;
+    fclose(in);
+    
+    return EXIT_SUCCESS;
 }
 
 void
